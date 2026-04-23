e7bfc4bc129467331dc25f2346f30d7d111714eb
KashiKoiAstra <okashi1421408023@outlook.com>
2026-04-23 16:21:57 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 osu-api4j
 
 osu! web api v2 wrapper for Java
+
+you should put your client id and secret in src/main/resources/config.properties
+
+```.properties
+client_id=12345
+client_secret=your_client_secret
+tokenUrl = https://osu.ppy.sh/oauth/token
+apiBaseUrl = https://osu.ppy.sh/api/v2
+scope = public
---
+++ src/main/java/kashi/koi/auth/AuthConfigLoader.java
@@ -0,0 +1,32 @@
+package kashi.koi.auth;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class AuthConfigLoader {
+
+    public static AuthConfig load(String resourcePath) {
+        Properties properties = new Properties();
+
+        try (var stream = AuthConfigLoader.class.getClassLoader().getResourceAsStream(resourcePath)) {
+            if (stream == null) throw  new IllegalArgumentException("Resource not found: " + resourcePath);
+
+            properties.load(stream);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load auth config from resource: " + resourcePath, e);
+        }
+
+        return new AuthConfig(
+                properties.getProperty("clientId"),
+                properties.getProperty("clientSecret"),
+                properties.getProperty("tokenUrl", "https://osu.ppy.sh/oauth/token"),
+                properties.getProperty("apiBaseUrl", "https://osu.ppy.sh/api/v2"),
+                properties.getProperty("scope", "public")
+        );
+    }
+
+    public static AuthConfig load() {
+        return load("auth.properties");
+    }
+}
